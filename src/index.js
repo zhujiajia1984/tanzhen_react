081cd5ebe56c149558197f3dd4603da745f16276
zhujiajia <772978409@qq.com>
2018-04-11 16:30:07 +0800

    分离css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './color.less';
 
 ReactDOM.render(
-	<h1>Hello, world!222</h1>,
+	<h1 className="textColor">Hello, world!222</h1>,
 	document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -21,6 +24,30 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
             }
+        }, {
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    { loader: "css-loader" },
+                    {
+                        loader: "less-loader",
+                        // options: {
+                        //     modifyVars: {
+                        //         '@primary-color': '#1DA57A',
+                        //     }
+                        // }
+                    }
+                ]
+            })
         }, ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'tanzhen',
+            template: './src/html/index.html',
+        })
+    ]
 }
