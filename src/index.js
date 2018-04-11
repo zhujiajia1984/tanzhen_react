00d3ec26377c3614338437d68a4fd745463d0300
zhujiajia <772978409@qq.com>
2018-04-11 22:16:06 +0800

    删除测试用less

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './color.less';
-import { Button } from 'antd';
+import App from './App';
 
 ReactDOM.render(
-	// <h1 className="textColor">Hello, world!222</h1>,
-	<div>
-	    <Button type="primary">Primary</Button>
-	    <Button>Default</Button>
-	    <Button type="dashed">Dashed</Button>
-	    <Button type="danger">Danger</Button>
-	</div>,
+	<App></App>,
 	document.getElementById('root')
 );
