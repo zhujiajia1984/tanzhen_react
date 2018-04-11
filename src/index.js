b59efe144b70ca83411bf4a1196926005822669b
zhujiajia <772978409@qq.com>
2018-04-11 17:33:22 +0800

    加载antd库

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './color.less';
+import { Button } from 'antd';
 
 ReactDOM.render(
-	<h1 className="textColor">Hello, world!222</h1>,
+	// <h1 className="textColor">Hello, world!222</h1>,
+	<div>
+	    <Button type="primary">Primary</Button>
+	    <Button>Default</Button>
+	    <Button type="dashed">Dashed</Button>
+	    <Button type="danger">Danger</Button>
+	</div>,
 	document.getElementById('root')
 );
