21090aeec68d0da4b254324c540c07a2040ff75b
zhujiajia <zhujiajia2003@163.com>
2018-05-14 14:21:42 +0800

    新建人群 add

--- src/pages/Market/MarketAdd.js
+++ src/pages/Market/MarketAdd.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import './MarketAdd.less';
+import { Steps, Divider, Button, message } from 'antd';
 import PageLayout from '../../components/PageLayout/PageLayout';
 
+// const
+const Step = Steps.Step;
+
+//
 export default class MarketAdd extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            current: 0,
+            steps: [{
+                title: '人群添加规则',
+                content: 'step1 content',
+            }, {
+                title: '确认人群规则',
+                content: 'step2 content',
+            }, {
+                title: '保存当前人群',
+                content: 'step3 content',
+            }],
+            btnLoading: false,
+        }
+    }
+
+    //
+    componentDidMount() {
+        let steps = this.state.steps;
+        steps[0].content = "111";
+        this.setState({ steps: steps });
+    }
+
+    //
+    onNext() {
+        const current = this.state.current + 1;
+        this.setState({ current: current });
+    }
+    onPrev() {
+        const current = this.state.current - 1;
+        this.setState({ current: current });
+    }
+    onSubmit() {
+        this.setState({ btnLoading: true });
+        setTimeout(() => {
+            message.success("人群保存成功", 1, () => {
+                this.setState({ btnLoading: false });
+                this.props.history.push("/market");
+            })
+        }, 2000)
     }
 
     render() {
@@ -12,7 +57,40 @@ export default class MarketAdd extends React.Component {
             <div>
             	<PageLayout	selMenu={['营销活动']}
 				>
-					<div className="marketAddTitleRow">新建人群</div>
+					<div className="marketAdd">
+						<div className="marketAddTitleRow">新建人群</div>
+						<Steps current={this.state.current}>
+							{this.state.steps.map(item => <Step key={item.title} title={item.title} />)}
+						</Steps>
+						<Divider />
+						<div className="steps-content">
+							{this.state.steps[this.state.current].content}
+						</div>
+						<Divider />
+						<div className="steps-action">
+						{
+							this.state.current < this.state.steps.length - 1
+							&&
+							<Button type="primary" onClick={this.onNext.bind(this)}>下一步</Button>
+						}
+						{
+							this.state.current === this.state.steps.length - 1
+							&&
+							<Button type="primary" onClick={this.onSubmit.bind(this)}
+								loading={this.state.btnLoading}
+							>
+								立即保存
+							</Button>
+						}
+						{
+							this.state.current > 0
+							&&
+							<Button style={{ marginLeft: 8 }} onClick={this.onPrev.bind(this)}>
+								上一步
+							</Button>
+						}
+						</div>
+					</div>
 				</PageLayout>
             </div>
         );
